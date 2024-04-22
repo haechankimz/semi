e22583e13398f03172fc1d03ebe9261f0913a3fb
hotncool <qqq366@naver.com>
2024-04-22 17:41:03 +0900

    boardList

---
+++ MusicProject/src/main/java/edu/kh/music/main/controller/MainController.java
@@ -0,0 +1,16 @@
+package edu.kh.music.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class MainController {
+
+	@RequestMapping("/")
+	public String mainPage() {
+		return "common/main";
+	}
+}

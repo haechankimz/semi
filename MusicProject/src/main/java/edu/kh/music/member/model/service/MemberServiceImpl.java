e2b8356d9e95690a4b04c44d65f038d95415cd8b
dlwltn <03dreams@naver.com>
2024-04-23 17:06:39 +0900

    로그인/회원가입 화면 구현 , 로그인 로그아웃 기능 구현

---
+++ MusicProject/src/main/java/edu/kh/music/member/model/service/MemberServiceImpl.java
@@ -0,0 +1,40 @@
+package edu.kh.music.member.model.service;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import edu.kh.music.member.model.dto.Member;
+import edu.kh.music.member.model.mapper.MemberMapper;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class MemberServiceImpl implements MemberService{
+	
+	private final MemberMapper mapper;
+	
+	private final BCryptPasswordEncoder bcrypt;
+	
+	// 로그인
+	@Override
+	public Member login(Member member) {
+		
+		// 입력 받은 pw
+//		String pw = member.getMemberPw();
+		
+		String bcryptPw = bcrypt.encode(member.getMemberPw());
+		
+		Member loginMember = mapper.login(member.getMemberEmail());
+		
+		if(loginMember == null) return null;
+		
+		if( !bcrypt.matches(member.getMemberPw(), loginMember.getMemberPw()) ) return null;
+		
+		loginMember.setMemberPw(null);
+		
+		return loginMember;
+	}
+
+}
---
+++ MusicProject/src/main/resources/static/js/main.js
@@ -0,0 +1,72 @@
+const popupClose = document.querySelector("#popupClose");
+const popupLayer = document.querySelector("#popupLayer");
+const loginForm = document.querySelector("#loginForm")
+
+if(loginForm != null) {
+    popupClose.addEventListener("click", () => {
+        popupLayer.classList.add("popup-hidden");
+    });
+}
+
+const loginIcon = document.querySelector("#loginIcon")
+if(loginIcon != null) {
+    loginIcon.addEventListener("click", ()=>{
+        popupLayer.classList.remove("popup-hidden");
+    });
+}
+
+/* 로그인 */
+const getCookie = key => {
+    const cookie = document.cookie;
+
+    const cookieList = cookie.split(";").map( temp => temp.split("="));
+
+    const obj = {};
+
+    for(let i = 0; i < cookieList.length; i++){
+        const k = cookieList[i][0];
+        const v = cookieList[i][1];
+        obj[k]=v;
+    }
+
+    return obj[key];
+}
+
+const loginEmail = document.querySelector("#loginForm input[name='memberEmail']");
+
+if(loginEmail != null){
+    const saveId = getCookie("saveId");
+
+    if(saveId != undefined) {
+        loginEmail.value=saveId;
+        document.querySelector("input[name='saveId']")
+    }
+}
+
+/* 이메일, 비밀번호 미작성 시 로그인 막기 */
+const loginPw = document.querySelector("#loginForm input[name='memberPw']");
+
+// 로그인 상태 아닐때
+if (loginForm != null) {
+
+    // 제출 이벤트 발생 시
+    loginForm.addEventListener("submit", e => {
+
+        // 이메일 미작성
+        if(loginEmail.value.trim().length === 0) {
+            alert("이메일을 작성해 주세요!");
+            e.preventDefault(); 
+            loginEmail.focus(); 
+            return;
+        }
+
+        // 비밀번호 미작성
+        if (loginPw.value.trim().length === 0) {
+            alert("비밀번호를 작성 해주세요!");
+            e.preventDefault(); 
+            loginPw.focus(); 
+            return;
+        }
+
+    });
+}

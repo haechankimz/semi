86832d33d953d476251ba11f8ea47b4344e3a3f5
hotncool <qqq366@naver.com>
2024-04-25 17:44:23 +0900

    진행중

--- MusicProject/src/main/resources/static/js/main.js
+++ MusicProject/src/main/resources/static/js/main.js
@@ -1,3 +1,26 @@
+/* banner swipe */
+var swiper = new Swiper(".swiper-container", {
+    slidesPerView: 1,
+    direction: 'horizontal',
+    autoplay: {
+        delay: 2000,
+    },
+    loop: true,
+    spaceBetween: 30,
+    centeredSlides: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    this.document.body.style.overflow="hidden";
+});
 
 const popupClose = document.querySelector("#popupClose");
 const popupLayer = document.querySelector("#popupLayer");
@@ -72,23 +95,4 @@ if (loginForm != null) {
     });
 }
 
-/* banner swipe */
-const swiper = new Swiper(".swiper-container", {
-    slidesPerview: 1,
-    direction: 'horizontal',
-    autoplay: {
-        delay: 2000,
-    },
-    loop: true,
-    spaceBetween: 30,
-    centeredSlides: true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-});
 

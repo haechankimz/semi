bceb36eaa272599ecdcd91e12ae832331bcc8de1
hotncool <qqq366@naver.com>
2024-04-29 14:33:38 +0900

    좋아요

--- MusicProject/src/main/resources/static/js/board/boardDetail.js
+++ MusicProject/src/main/resources/static/js/board/boardDetail.js
@@ -9,14 +9,37 @@ goListBtn.addEventListener("click", () => {
 })
 
 
-/* 카테고리 버튼에 따라 리스트 뜨게 하기 */
-// const categoryBtn = document.querySelectorAll(".category-btn");
+const boardLike = document.querySelector("#boardLike");
+boardLike.addEventListener("click", e => {
 
-// categoryBtn.forEach(categoryList => {
-//     categoryList.addEventListener("click", () => {
-//         const categoryNo = categoryList.dataset.categoryNo;
-//         const boardCode = categoryList.dataset.boardCode;
-//         const url = `/board/${boardCode}?categoryNo=${categoryNo}`;
-//         window.location.href = url;
-//     });
-// });
+    if(loginMemberNo == null){
+        alert("로그인 후 좋아요를 눌러주세요");
+        return;
+    }
+
+    const obj = {
+        "memberNo" : loginMemberNo,
+        "boardNo" : boardNo,
+        "likeCheck" : likeCheck
+    };
+
+    fetch("/board/like", {
+        method : "POST",
+        headers : {"Content-Type" : "application/json"},
+        body : JSON.stringify(obj)
+    })
+    .then(resp => resp.text())
+    .then(count => {
+        if(count == -1){
+            console.log("좋아요 처리 실패");
+            return;
+        }
+
+        likeCheck = likeCheck == 0 ? 1 : 0;
+
+        e.target.classList.toggle("fa-regular");
+        e.target.classList.toggle("fa-solid");
+
+        e.target.nextElementSibling.innerText = count;
+    })
+});

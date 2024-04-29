187b54e75ef6893e6dc1be335a5efc9a411bc672
dlwltn <03dreams@naver.com>
2024-04-29 09:04:29 +0900

    빠른 로그인 수정

--- MusicProject/src/main/resources/static/js/main.js
+++ MusicProject/src/main/resources/static/js/main.js
@@ -98,16 +98,11 @@ if (loginForm != null) {
 
 
 //  빠른 로그인
-const quickLoginBtns = document.querySelectorAll(".quick-login");
+const quickLoginBtn = document.querySelector(".quick-login");
 
-quickLoginBtns.forEach((item, index) => {
-    // item : 현재 반복 시 꺼내온 객체
-    // index : 현재 반복 중인 인덱스
+// quickLoginBtns 요소를 하나씩 꺼내서 이벤트 리스너 추가
+quickLoginBtn.addEventListener("click", () => {
+    const email = quickLoginBtn.innerText; // 버튼에 작성된 이메일 얻어오기
+    location.href = "/member/quickLogin?memberEmail=" + email;
 
-    // quickLoginBtns 요소를 하나씩 꺼내서 이벤트 리스너 추가
-    item.addEventListener("click", e => {
-        const email = item.innerText; // 버튼에 작성된 이메일 얻어오기
-        location.href="/member/quickLogin?memberEmail=" + email;
-
-    });
 });
